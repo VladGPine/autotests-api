2a98b961d677eb48f46f741ddd229f86e41b4d2d
VladGPine <shikarno_3000@inbox.ru>
2026-01-17 20:54:45 +0300

    Валидация JSON schema. Практикуемся в работе с JSON schema

---
+++ pydantic_json_schema_get_user.py
@@ -0,0 +1,28 @@
+from clients.private_http_builder import AuthenticationUserSchema
+from clients.users.private_users_client import get_private_users_client
+from clients.users.public_users_client import get_public_users_client
+from clients.users.users_schema import CreateUserRequestSchema, GetUserResponseSchema
+from tools.assertions.schema import validate_json_schema
+from tools.fakers import get_random_email
+
+
+public_users_client = get_public_users_client()
+
+create_user_request = CreateUserRequestSchema(
+    email=get_random_email(),
+    password='string',
+    last_name='string',
+    first_name='string',
+    middle_name='string'
+)
+create_user_response = public_users_client.create_user(create_user_request)
+private_user_request = AuthenticationUserSchema(
+    email=create_user_request.email,
+    password=create_user_request.password
+)
+private_user_client = get_private_users_client(private_user_request)
+get_private_user_response = private_user_client.get_user_api(
+    user_id=create_user_response.user.id
+)
+
+validate_json_schema(instance=get_private_user_response.json(), schema=GetUserResponseSchema.model_json_schema())
